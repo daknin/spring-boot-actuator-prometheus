20bbbd4a905682d5dc07acf25ffad35baa85d992
Darrell King <kingdarrella@gmail.com>
2016-11-19 16:49:52 +0000

    Initial commit

---
+++ src/main/java/com/github/daknin/SpringBootActuatorPrometheusApplication.java
@@ -0,0 +1,12 @@
+package com.github.daknin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActuatorPrometheusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootActuatorPrometheusApplication.class, args);
+	}
+}
