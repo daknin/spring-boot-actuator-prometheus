0410d2be460e4e69e2c81a374b93cd84f4eb88d3
Darrell King <kingdarrella@gmail.com>
2016-11-20 08:14:13 +0000

    Use annotations and added security

---
+++ Readme.md
@@ -0,0 +1,6 @@
+Simple Spring Boot application that exposes Spring Actuator metrics on a Prometheus endpoint.
+
+Metrics are available at
+	```
+	http://localhost:8080/prometheus
+	```
--- src/main/java/com/github/daknin/SpringBootActuatorPrometheusApplication.java
+++ src/main/java/com/github/daknin/SpringBootActuatorPrometheusApplication.java
@@ -1,12 +1,16 @@
 package com.github.daknin;
 
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableSpringBootMetricsCollector
+@EnablePrometheusEndpoint
 public class SpringBootActuatorPrometheusApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootActuatorPrometheusApplication.class, args);
-	}
+	public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(SpringBootActuatorPrometheusApplication.class, args);
+    }
 }
